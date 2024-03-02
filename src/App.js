1bd985327982e33ab7fcf06ae9fa390abca22571
Kiran Anand <liteninkiran@hotmail.com>
2024-03-02 16:18:08 +0000

    011 - Conditionals in JSX - RESET

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
 function App() {
-
-    const title = 'Blog Post';
-    const body = 'This is my blog post';
-    const comments = [
-        { id: 1, text: 'Comment 1' },
-        { id: 2, text: 'Comment 2' },
-        { id: 3, text: 'Comment 3' },
-    ];
-    const loading = false;
-    const showComments = true;
-    const commentsHtml = (
-        <div className="comments">
-            <h3>Comments ({ comments.length })</h3>
-            <ul>
-                { comments.map((comment, index) => (<li key={ index }>{ comment.text }</li>)) }
-            </ul>
-        </div>
-    );
-
-    if (loading) {
-        return (<h1>Loading...</h1>);
-    }
-
     return (
         <div className="container">
-            <h1>{ title }</h1>
-            <p>{ body }</p>
-            { showComments && commentsHtml }
+            <h1>My App</h1>
         </div>
     );
 }
