a56bb69acba57d7c96c42597f228856c578e2cda
Kiran Anand <liteninkiran@hotmail.com>
2024-03-02 16:10:00 +0000

    010 - Dynamic Values & Lists in JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 function App() {
+
+    const title = 'Blog Post';
+    const body = 'This is my blog post';
+    const comments = [
+        { id: 1, text: 'Comment 1' },
+        { id: 2, text: 'Comment 2' },
+        { id: 3, text: 'Comment 3' },
+    ];
+
     return (
         <div className="container">
-            <h1>My App</h1>
+            <h1>{ title }</h1>
+            <p>{ body }</p>
+
+            <div className="comments">
+                <h3>Comments ({ comments.length })</h3>
+                <ul>{ comments.map((comment, index) => (<li key={ index }>{ comment.text }</li>)) }
+                </ul>
+            </div>
+
         </div>
     );
 }
