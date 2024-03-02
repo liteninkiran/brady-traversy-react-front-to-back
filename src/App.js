b0200d883552533b58caac161429a53df847e7ba
Kiran Anand <liteninkiran@hotmail.com>
2024-03-02 16:17:10 +0000

    011 - Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ function App() {
         { id: 2, text: 'Comment 2' },
         { id: 3, text: 'Comment 3' },
     ];
+    const loading = false;
+    const showComments = true;
+    const commentsHtml = (
+        <div className="comments">
+            <h3>Comments ({ comments.length })</h3>
+            <ul>
+                { comments.map((comment, index) => (<li key={ index }>{ comment.text }</li>)) }
+            </ul>
+        </div>
+    );
+
+    if (loading) {
+        return (<h1>Loading...</h1>);
+    }
 
     return (
         <div className="container">
             <h1>{ title }</h1>
             <p>{ body }</p>
-
-            <div className="comments">
-                <h3>Comments ({ comments.length })</h3>
-                <ul>{ comments.map((comment, index) => (<li key={ index }>{ comment.text }</li>)) }
-                </ul>
-            </div>
-
+            { showComments && commentsHtml }
         </div>
     );
 }
