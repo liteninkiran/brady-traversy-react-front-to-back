43e98cd40659628f784ad4da8e8f466920b41017
Kiran Anand <liteninkiran@hotmail.com>
2024-03-03 11:25:49 +0000

    018 - FeedbackStats Component & Reactivity

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -6,14 +6,6 @@ function Card({ children, reverse }) {
             { children }
         </div>
     );
-    // return (
-    //     <div className='card' style={{
-    //         backgroundColor: reverse ? 'rgba(0, 0, 0, 0.4)' : '#fff',
-    //         color: reverse ? '#fff' : '#000',
-    //     }}>
-    //         { children }
-    //     </div>
-    // );
 }
 
 Card.defaultProps = {
