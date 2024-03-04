c5db81eaea6e3768cba24db88f8f5e10e79026b6
Kiran Anand <liteninkiran@hotmail.com>
2024-03-04 10:32:34 +0000

    032 - Moving Functions To Context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { FeedbackProvider } from './context/FeedbackContext';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
-import FeedbackData from './data/FeedbackData';
 import AboutPage from './pages/AboutPage';
 import AboutIconLink from './components/AboutIconLink';
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData);
-    const deleteFeedback = (id) => {
-        if (window.confirm('Are you sure you want to delete?')) {
-            setFeedback(feedback.filter((item) => item.id !== id));
-        }
-    }
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
-    }
 
     return (
         <FeedbackProvider>
@@ -31,9 +18,9 @@ function App() {
                         <Route exact path='/' element={
                             <>
                                 <AboutIconLink />
-                                <FeedbackForm handleAdd={ addFeedback } />
+                                <FeedbackForm />
                                 <FeedbackStats />
-                                <FeedbackList handleDelete={ deleteFeedback } />
+                                <FeedbackList />
                             </>
                         }>
                         </Route>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
 import RatingSelect from './RatingSelect';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackForm({ handleAdd }) {
+function FeedbackForm() {
+    const { addFeedback } = useContext(FeedbackContext);
     const [rating, setRating] = useState(10);
     const [text, setText] = useState('');
     const [btnDisabled, setBtnDisabled] = useState(true);
@@ -28,7 +30,7 @@ function FeedbackForm({ handleAdd }) {
                 text,
                 rating,
             }
-            handleAdd(newFeedback);
+            addFeedback(newFeedback);
             setText('');
         }
     }
