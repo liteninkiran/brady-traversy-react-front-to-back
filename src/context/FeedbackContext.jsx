65961cc8a8d69e0ee2fa8013d499f32d1480861f
Kiran Anand <liteninkiran@hotmail.com>
2024-03-04 17:43:25 +0000

    043 - Update & Delete From JSON-Server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -16,9 +16,7 @@ export const FeedbackProvider = ({ children }) => {
         const url = '/feedback';
         const options = {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(newFeedback),
         }
         const response = await fetch(url, options);
@@ -26,8 +24,11 @@ export const FeedbackProvider = ({ children }) => {
         setFeedback([data, ...feedback]);
     }
 
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
+            const url = `/feedback/${id}`;
+            const options = { method: 'DELETE' }
+            await fetch(url, options);
             setFeedback(feedback.filter((item) => item.id !== id));
         }
     }
@@ -36,8 +37,16 @@ export const FeedbackProvider = ({ children }) => {
         setFeedbackEdit({ item, edit: true});
     }
 
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item));
+    const updateFeedback = async (id, updItem) => {
+        const url = `/feedback/${id}`;
+        const options = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(updItem),
+        }
+        const response = await fetch(url, options);
+        const data = await response.json();
+        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item));
     }
 
     useEffect(() => {
